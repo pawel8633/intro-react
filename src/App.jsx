468a678fa48322fb609ed996dbdfd7702f30f260
LAPTOP-1821QFF9\krack <kracker86@interia.pl>
2020-04-22 12:49:13 +0200

    change props for students rendering

--- src/App.jsx
+++ src/App.jsx
@@ -71,16 +71,16 @@ this.setState({
 
     /* Change below to render students*/
 
-    /*if (this.state.buttonClicked === "students") {
+    if (this.state.buttonClicked === "students") {
       tabChoice = (
         <List
-          placeholder="Add Assignment..." 
-          currList={this.state.assignments}
-          addFunction={this.addAssignment}
+          placeholder="Add Stundent..." 
+          currList={this.state.students}
+          addFunction={this.addStudent}
           title="Student Roster"
         />
       );
-    }*/
+    }
 
     /* Uncomment lines below to render grades*/
     /*if (this.state.buttonClicked === "grades") {
